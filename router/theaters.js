8d84147c8b87b5014f9ea3007ef3a5bf31edb1be
Kartik Panjla <kartik198200023@gmail.com>
2021-09-26 13:28:50 +0530

    third

--- router/theaters.js
+++ router/theaters.js
@@ -4,7 +4,7 @@ const router=express.Router();
 const services=require("../Services/theater")
 
 router.get("/",services.get);
-router.get("/:id",services.getOne);
+//router.get("/:id",services.getOne);
 router.post("/",services.addTheater);
 router.put("/:id",services.editTheater);
 router.delete("/:id",services.deleteTheater);
